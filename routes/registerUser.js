4c9d50781479b0911aace898bc02a9670bca2b33
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-05-05 19:26:30 -0400

    Backend quase tudo pronto

---
+++ routes/registerUser.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import prisma from "../db/index.js";
+
+const registerRouter = Router();
+
+registerRouter.post("/register", async (req, res) => {
+    console.log("Requisição de register recebida:", req.body);
+    const { name, email, password } = req.body;
+    try {
+        const user = await prisma.user.create({
+        data: {name, email, password}
+        });
+        res.status(200).json({ message: "Registro bem-sucedido!" });
+    } catch (err) {
+        console.error("Error in register:", err);
+        res.status(500).json({ message: "Erro interno do servidor", error: err.message });
+    }
+});
+
+export default registerRouter;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import userRouter from "./routes/userRoutes.js"
 import loginRouter from "./routes/loginUser.js"
+import registerRouter from "./routes/registerUser.js"
 import cors from "cors"
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(express.json())
 
 app.use("/users", userRouter)
 app.use("/auth", loginRouter)
+app.use("/auth", registerRouter)
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
