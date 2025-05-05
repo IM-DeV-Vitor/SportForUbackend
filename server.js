b5b6ce7e37e1e610e187d1b6b32c3daacdc68c2a
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-05-05 14:44:59 -0400

    POrta

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import loginRouter from "./routes/loginUser.js"
 import cors from "cors"
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.use(cors())
 app.use(express.json())
