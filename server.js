9fd20a09207527b7da1f294d0cef107d9f40193e
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-05-27 21:36:07 -0400

    Erro de CORS corrigido

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ import cors from "cors"
 const app = express()
 const port = process.env.PORT || 8080
 
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:5173", "https://sport-for-u.vercel.app"], 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true
+}));
+
 app.use(express.json())
 
 app.use("/users", userRouter)
