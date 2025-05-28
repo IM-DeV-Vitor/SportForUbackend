fbb672fd5f0e3c95a587a07dddfc2d7ebd584de9
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-05-27 21:48:10 -0400

    Problems...

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ import cors from "cors"
 const app = express()
 const port = process.env.PORT || 8080
 
-app.use(cors({
-  origin: ["http://localhost:5173", "https://sport-for-u.vercel.app"], 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type"],
-  credentials: true
-}));
-
 app.options("*", cors());
 
 app.use(express.json())
