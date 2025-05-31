9114e0e55b597a3af712d7e087e83d18c658a198
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-05-30 23:02:39 -0400

    Added: Try to solve error again

--- routes/loginUser.js
+++ routes/loginUser.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import prisma from "../db/index.js";
+import jwt from "jsonwebtoken";
 
 const loginRouter = Router();
 
 loginRouter.post("/login", async (req, res) => {
     console.log("Requisição de login recebida:", req.body);
     const { email, password } = req.body;
+
     try {
         const user = await prisma.user.findUnique({
             where: { email }
@@ -15,7 +17,11 @@ loginRouter.post("/login", async (req, res) => {
             return res.status(401).json({ message: "Credenciais inválidas" });
         }
 
-        res.status(200).json({ message: "Login bem-sucedido!" });
+        const token = jwt.sign({ id: user.id }, "pintoEnormeNaBocaDoIsaque", {
+            expiresIn: "1d" 
+        });
+
+        res.status(200).json({ token });
     } catch (err) {
         console.error("Erro no login:", err);
         res.status(500).json({ message: "Erro interno do servidor", error: err.message });
