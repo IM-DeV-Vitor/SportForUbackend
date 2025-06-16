c8c73f4a4724af07d3ad0367a3403acaa20cc187
Vitor <vitorhugo1235lll@gmail.com>
2025-06-15 20:42:21 -0400

    New feature

--- routes/loginUser.js
+++ routes/loginUser.js
@@ -28,5 +28,33 @@ export default function loginRouterFactory(prisma) {
     }
   });
 
+  loginRouter.post("/login/google", async (req, res) => {
+    const { name, email } = req.body;
+
+    try {
+      let user = await prisma.user.findUnique({ where: { email } });
+
+      if (!user) {
+        user = await prisma.user.create({
+          data: { name, email, password: "" }, 
+        });
+      }
+
+      const token = jwt.sign({ id: user.id }, "pintoEnormeNaBocaDoIsaque", {
+        expiresIn: "1d",
+      });
+
+      res.status(200).json({
+        token,
+        userId: user.id,
+        name: user.name,
+        email: user.email,
+      });
+    } catch (err) {
+      console.error("Erro no login com Google:", err);
+      res.status(500).json({ message: "Erro interno do servidor", error: err.message });
+    }
+  });
+
   return loginRouter;
 }
