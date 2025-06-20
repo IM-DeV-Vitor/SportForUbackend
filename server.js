2d6a1fe3611e54d62adc0db1695e306613470006
Vitor <vitorhugo1235lll@gmail.com>
2025-06-20 00:05:14 -0400

    Added: Now distance delete in final of day

--- server.js
+++ server.js
@@ -5,17 +5,37 @@ import userRouterFactory from "./routes/userRoutes.js";
 import loginRouterFactory from "./routes/loginUser.js";
 import registerRouterFactory from "./routes/registerUser.js";
 import walkRouterFactory from "./routes/walkedDistance.js";
+import cron from "node-cron";
 
 const app = express();
 const port = process.env.PORT || 8080;
 const prisma = new PrismaClient();
 
-app.use(cors({
-  origin: ["http://localhost:5173", "https://sport-for-u.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+cron.schedule("5 0 * * *", async () => {
+  console.log("Executando tarefa diária de limpeza de dados...");
+
+  const now = new Date();
+  const today = new Date(
+    Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate())
+  );
+
+  const result = await prisma.dailyDistance.deleteMany({
+    where: {
+      date: {
+        lt: today,
+      },
+    },
+  });
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://sport-for-u.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/auth", loginRouterFactory(prisma));
@@ -33,7 +53,7 @@ app.listen(port, async () => {
   }
 });
 
-process.on('beforeExit', async () => {
+process.on("beforeExit", async () => {
   await prisma.$disconnect();
   console.log("Desconectado do banco de dados.");
 });
